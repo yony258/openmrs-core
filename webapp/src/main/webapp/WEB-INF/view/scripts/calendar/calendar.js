c2f0b721f32f5db2ebab3fb786dcb646c2023bdf
yony258 <yonatan.proj@gmail.com>
2012-12-04 16:44:13 +0200

    Added a new function the calender.js in order to support future dates (Will be used in Appointments Module (AM Project) and hopefully in more modules)

--- webapp/src/main/webapp/WEB-INF/view/scripts/calendar/calendar.js
+++ webapp/src/main/webapp/WEB-INF/view/scripts/calendar/calendar.js
@@ -23,3 +23,34 @@ function showCalendar(obj, yearsPrevious) {
 	dp.show();
 	return false;
 }
+
+function showCalendar(obj, yearsPrevious, yearsNext) {
+    // if the object doesn't have an id, just set it to some random text so jq can use it
+    if(!obj.id) {
+            obj.id = "something_random" + (Math.random()*1000);
+    }
+   
+    //set appendText to something so it doesn't automagically pop into the page
+    var opts = { appendText: " " };
+    
+  //set yearPrevious to 110 if it has'nt been parsed in an argument
+    if (!yearsPrevious){
+		yearsPrevious= 110;
+	}
+    
+  //set yearsNext to 10 if it has'nt been parsed in an argument
+    if (!yearsNext){
+		yearsNext= 10;
+	}
+    
+    if (yearsPrevious && yearsNext)
+            opts["yearRange"] = "-"+yearsPrevious+":+"+yearsNext;
+
+    alert(opts["yearRange"]);
+    var dp = new DatePicker(jsDateFormat, obj.id, opts);
+    jQuery.datepicker.setDefaults(jQuery.datepicker.regional[jsLocale]);
+   
+    obj.onclick = null;
+    dp.show();
+    return false;
+}
